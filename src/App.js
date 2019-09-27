8716de8db7c8a84f1419261bb0a3c4c2b318f67c
lbhsos <lbhsos29@naver.com>
2019-09-27 15:56:16 +0900

    extra attributes and mixins

--- src/App.js
+++ src/App.js
@@ -7,13 +7,25 @@ createGlobalStyle`
     margin:0;
   }
 `;
+const awesomeCard= css`
+  box-shadow: 0 4px 6px rgba(50,50,93,0.11). 0 1px 3px rgba(0,0,0,0.08);
+  background-color: white;
+  border-radius:10px;
+  padding: 20px;
+`;
+
+const Input = styled.input.attrs({
+  required: true
+})`
+  border:none;
+  ${awesomeCard};
+`;
+
 class App extends Component{
   render(){
     return (
       <Container>
-        <Button success>Hello</Button>
-        <Button danger roatationTime={5}>Hello</Button>
-        <Anchor href="http://google.co.kr">Go to google</Anchor>
+        <Input></Input>
       </Container>
     );
   }
@@ -22,38 +34,5 @@ const Container = styled.div`
   height:100vh;
   width: 100%;
   background-color: pink;
-`
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight : 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus{
-    outline:none;
-  }
-  background-color: ${props => props.danger? "#e74c3c" : "#2ecc71"};
-  ${props => {
-    if(props.danger){
-      return css`animation: ${rotation} ${props.rotationsTime} linear infinite`
-    }
-  }}
-`;
-
-//extension button
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration:none;
-`;
-
-const rotation = keyframes`
-  from{
-    transform: rotate(0deg);
-  }
-  to{
-    transform: rotate(360deg);
-  }
 `
 export default App;
