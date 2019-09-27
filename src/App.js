61607a555cfa67ae2864d98098821ed50b67b9c1
lbhsos <lbhsos29@naver.com>
2019-09-27 16:06:44 +0900

    theming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component, Fragment} from 'react';
-import styled, { createGlobalStyle,css, keyframes} from "styled-components";
+import React, { Component } from 'react';
+import styled, { createGlobalStyle,css, keyframes, ThemeProvider} from "styled-components";
+import theme from "./theme";
 
 createGlobalStyle`
   body{
@@ -7,32 +8,32 @@ createGlobalStyle`
     margin:0;
   }
 `;
-const awesomeCard= css`
-  box-shadow: 0 4px 6px rgba(50,50,93,0.11). 0 1px 3px rgba(0,0,0,0.08);
-  background-color: white;
-  border-radius:10px;
-  padding: 20px;
-`;
-
-const Input = styled.input.attrs({
-  required: true
-})`
-  border:none;
-  ${awesomeCard};
-`;
-
-class App extends Component{
-  render(){
-    return (
-      <Container>
-        <Input></Input>
-      </Container>
-    );
-  }
-}
 const Container = styled.div`
   height:100vh;
   width: 100%;
   background-color: pink;
 `
+
+const Card = styled.div`
+  background-color:red;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props=>props.theme.successColor};
+`
+class App extends Component{
+  render(){
+    return (
+      <ThemeProvider theme={theme}>
+      <Container>
+        <Form />
+      </Container>
+      </ThemeProvider>
+    );
+  }
+}
+
+const Form = () => (<Card><Button>Hello</Button></Card>)
 export default App;
